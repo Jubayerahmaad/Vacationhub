a2a3b42422b4f3e38e3ac76d423f9aa8e42fccc9
Jubayerahmaad <jubuofficial84@gmail.com>
2024-10-03 17:32:30 +0600

    Swipe Js Slide Added

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,14 @@
 
 $('.counter').counterUp();
 
+// swipe slide innisilize
+
+var swiper = new Swiper(".myswiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
