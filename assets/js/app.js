78e8d3fb1d0efcfcdbdff825b92b6dad27a874cd
Jubayerahmaad <jubuofficial84@gmail.com>
2024-10-04 01:01:13 +0600

    Testimonial Slide added

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,26 @@
 
 $('.counter').counterUp();
 
-// swipe slide innisilize
+// swipe slide innisilize tour place 
 
 var swiper = new Swiper(".myswiper", {
     slidesPerView: 3,
     spaceBetween: 30,
+    grabCursor: 'true',
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  // testimonial slide 
+
+  var swiper = new Swiper(".myswipe", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centerSlide: 'true',
+    grabCursor: 'true',
 
     navigation: {
       nextEl: ".swiper-button-next",
