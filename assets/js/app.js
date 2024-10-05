1b2589f0a46756bd12261b9071d8cba72937fafb
Jubayerahmaad <jubuofficial84@gmail.com>
2024-10-05 13:50:22 +0600

    Befor Final Touch

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,11 +8,28 @@ var swiper = new Swiper(".myswiper", {
     slidesPerView: 3,
     spaceBetween: 30,
     grabCursor: 'true',
-
+    
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+
+    breakpoints: {
+
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      
+      600: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      
+      950: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }}
   });
 
   // testimonial slide 
@@ -27,4 +44,27 @@ var swiper = new Swiper(".myswiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+
+    breakpoints: {
+
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      
+      600: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      
+      950: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }}
   });
+
+  /* Humburger Navbar*/
+  function menuToggoler(){
+    var navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("open");
+  }
